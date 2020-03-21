2421cf4616d2e48f2f63fc08eee63e22b27fbc4e
Holly <holly.jo.robertson22@gmail.com>
2020-03-21 18:26:57 -0500

    Testing before password authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
+
   get 'login', to: 'sessions#new'
+
   post 'login', to: 'sessions#create'
+
   get 'signup', to: 'users#new'
+
   get 'welcome', to: 'sessions#welcome'
+
   delete 'logout', to: 'sessions#destroy'
+
   post 'file_manager/download_accountmanager', to: 'file_manager#download_accountmanager'
+
   root 'sessions#welcome'
 end
