d78d8d1df2689034890ec925cd09fd774bf85428
Holly <holly.jo.robertson22@gmail.com>
2020-03-21 20:43:02 -0500

    UI done

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'bootstrap-sass', '3.2.0.0'
+gem 'bootstrap-sass'
 # To encrypt a user's password
 gem 'bcrypt'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :name, :email
+  attr_accessor :username, :password
   has_secure_password
-  def initialize(attributes = {})
-    @name  = attributes[:name]
-    @email = attributes[:email]
-  end
-
-  def formatted_email
-    "#{@name} <#{@email}>"
-  end
+  
 end
