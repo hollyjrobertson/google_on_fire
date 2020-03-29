0c9507208df64e04d4a467d0a4112e3bc8d433f6
Holly <holly.jo.robertson22@gmail.com>
2020-03-29 12:27:34 -0500

    Edited gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 80 })
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  def gravatar_for(user, options = { size: 100 })
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_id = user.id * size
+    gravatar_url = "https://i.picsum.photos/id/#{gravatar_id}/#{size}/#{size}.jpg"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
