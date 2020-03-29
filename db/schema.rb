9737f2b8ac3ad6fb7a3ff9f88f1add9ba7e47e43
Holly <holly.jo.robertson22@gmail.com>
2020-03-29 09:55:30 -0500

    Secure Password

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200328230136) do
+ActiveRecord::Schema.define(version: 20200329143701) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
