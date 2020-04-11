75ca859752534990dc1611e5f96bfec193998cd7
Holly <holly.jo.robertson22@gmail.com>
2020-04-11 16:06:24 -0500

    Added Chore Database

--- app/helpers/chores_helper.rb
+++ app/helpers/chores_helper.rb
@@ -1,2 +1,15 @@
 module ChoresHelper
+  #Sets Border Color based on Availability
+  def due_date(chore)
+    Date.parse(chore.completeBy.to_s).strftime("%B %e, %Y")
+  end
+  def border_class(chore)
+    if chore.isCheckedOut == true
+      "orange"
+    elsif chore.isCheckedOut == false
+      "green"
+    else
+      "red"
+    end
+  end
 end
