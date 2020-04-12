6effe9f8bddca5a89a7de57d3192d8e123b3bdcd
Holly <holly.jo.robertson22@gmail.com>
2020-04-12 14:13:40 -0500

    Final

--- app/controllers/file_manager_controller.rb
+++ app/controllers/file_manager_controller.rb
@@ -1,40 +1,44 @@
 class FileManagerController < ApplicationController
 
     #First File to download - Google Account Manager APK
-    #Version 7.1.2
+    #Version 5.1-1743759
+    #https://www.apkmirror.com/apk/google-inc/google-account-manager/google-account-manager-5-1-1743759-release/google-account-manager-5-1-1743759-android-apk-download/
     def download_accountmanager
     send_file(
-        "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=97485",
+        "app/assets/files/first_accountmanager.apk",
         filename: "Install 1st",
         type: "application/vnd.android.package-archive"
     )
     end
 
     #Second File to download - Google Services Framework APK
-    #Version 10_5771379
+    #Version 5.1-1743759
+    #https://www.apkmirror.com/apk/google-inc/google-services-framework/google-services-framework-5-1-1743759-release/google-services-framework-5-1-1743759-android-apk-download/
     def download_servicesframework
     send_file(
-        "app/assets/files/GoogleServicesFramework_10_5771379.apk",
+        "app/assets/files/second_servicesframework.apk",
         filename: "Install 2nd",
         type: "application/vnd.android.package-archive"
     )
     end
 
     #Third File to download - Google Play Services APK
-    #Version 20.06.15_120400
+    #Version 11.5.09 (240-164803921)
+    #https://www.apkmirror.com/apk/google-inc/google-play-services/google-play-services-11-5-09-release/google-play-services-11-5-09-240-164803921-android-apk-download/
     def download_playservices
       send_file(
-          "app/assets/files/GooglePlayServices_20.06.15_120400.apk",
+          "app/assets/files/third_playservices.apk",
           filename: "Install 3rd",
           type: "application/vnd.android.package-archive"
       )
     end
 
     #Fourth File to download - Google Play Store APK
-    #Version 18.8.16
+    #Version 7.0.25.H-all [0] (noarch) (nodpi)
+    #https://www.apkmirror.com/apk/google-inc/google-play-store/google-play-store-7-0-25-h-all-0-release/google-play-store-7-0-25-h-0-android-apk-download/
     def download_playstore
       send_file(
-          "app/assets/files/GooglePlayStore_18.8.16.apk",
+          "app/assets/files/fourth_playstore.apk",
           filename: "Install 4th",
           type: "application/vnd.android.package-archive"
       )
