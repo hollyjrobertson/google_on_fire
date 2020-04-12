b7e4b5a6be080ba9a9772983b1a68edc5bb22ec3
Holly <holly.jo.robertson22@gmail.com>
2020-04-12 10:24:55 -0500

    Added all file links

--- app/controllers/file_manager_controller.rb
+++ app/controllers/file_manager_controller.rb
@@ -1,46 +1,42 @@
 class FileManagerController < ApplicationController
 
-#Second File to download - Google Services Framework APK
-  #Version 10_5771379
-  def download_servicesframework
+    #First File to download - Google Account Manager APK
+    #Version 7.1.2
+    def download_accountmanager
     send_file(
-        "app/assets/files/GoogleServicesFramework_10_5771379.apk",
-        filename: "File 2 - Google Services Framework APK.apk",
+        "app/assets/files/GoogleAccountManager_7.1.2.apk",
+        filename: "Install 1st",
         type: "application/vnd.android.package-archive"
     )
-  end
+    end
 
-  #Third File to download - Google Play Services APK
+    #Second File to download - Google Services Framework APK
+    #Version 10_5771379
+    def download_servicesframework
+    send_file(
+        "app/assets/files/GoogleServicesFramework_10_5771379.apk",
+        filename: "Install 2nd",
+        type: "application/vnd.android.package-archive"
+    )
+    end
+
+    #Third File to download - Google Play Services APK
     #Version 20.06.15_120400
     def download_playservices
       send_file(
           "app/assets/files/GooglePlayServices_20.06.15_120400.apk",
-          filename: "File 3 - Google Play Services APK.apk",
+          filename: "Install 3rd",
           type: "application/vnd.android.package-archive"
       )
     end
 
-  #Fourth File to download - Google Play Store APK
+    #Fourth File to download - Google Play Store APK
     #Version 18.8.16
     def download_playstore
       send_file(
           "app/assets/files/GooglePlayStore_18.8.16.apk",
-          filename: "File 3 - Google Play Services APK.apk",
+          filename: "Install 4th",
           type: "application/vnd.android.package-archive"
       )
     end
-
-  #First File to download - Google Account Manager APK
-  #Version 7.1.2
-  def download_accountmanager
-    send_file(
-        "app/assets/files/GoogleAccountManager_7.1.2.apk",
-        filename: "File 1 - Google Account Manager APK.apk",
-        type: "application/vnd.android.package-archive"
-    )
-  end
-
-
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,13 @@ Rails.application.routes.draw do
 
   get 'contact' => 'static_pages#contact'
 
-  post 'download' => 'file_manager#download_accountmanager'
+  post 'first' => 'file_manager#download_accountmanager'
+
+  post 'second' => 'file_manager#download_servicesframework'
+
+  post 'third' => 'file_manager#download_playservices'
+
+  post 'fourth' => 'file_manager#download_playstore'
 
   get    'login'   => 'sessions#new'
   
